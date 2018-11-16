f07d63a7052adf9f3313d131dcaa2534d3f72877
romanrozin <r@commit.com>
2018-11-16 21:24:46 +0200

    dfdf

---
+++ Readme.md
@@ -0,0 +1,12 @@
+## This is sparta
+# this 
+
+
+```python
+def moshe():
+    print ("x")
+    for x in range(1):
+        print(x)
+```
+
+
---
+++ app2.py
@@ -0,0 +1,34 @@
+from flask import Flask
+app = Flask(__name__)
+import os
+
+fileName = "tmpfile.txt"
+
+if not os.path.exists(fileName):
+    with open(fileName, "w+") as file:
+        file.write("0")
+
+
+@app.route('/change',methods=['POST', 'GET'])
+def change_func_page():
+    with open(fileName, "r") as file:
+        data = file.read()
+
+    with open(fileName, "w+") as file:
+        if "1" in data:
+            val = "0"
+            file.write(val)
+        else:
+            val = "1"
+            file.write(val)
+    return (val)
+
+@app.route('/')
+def hello():
+    with open(fileName, "r") as file:
+        data = file.read()
+    return data
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host="0.0.0.0")
+
