8bb37a409cdac26bae1b81a66f2d09d1990b1c2f
romanrozin <r@commit.com>
2018-11-16 19:49:07 +0200

    dfdf

--- local/control_led.py
+++ local/control_led.py
@@ -11,13 +11,15 @@ GPIO.setup(18, GPIO.OUT)
 if __name__ == '__main__':
     try:
         while (True):
+            start = time.time()
             with requests.get("https://still-crag-29020.herokuapp.com/") as url:
-
+                end = time.time() - start
+                print("req took : " , end )
                 if "1" in url.text:
-                    print("LED on " , datetime.datetime.now())
+                    print("LED on : " , datetime.datetime.now())
                     GPIO.output(18, GPIO.HIGH)
                 if "0" in url.text:
-                    print("LED off " , datetime.datetime.now())
+                    print("LED off : " , datetime.datetime.now())
                     GPIO.output(18, GPIO.LOW)
 
     except KeyboardInterrupt:
