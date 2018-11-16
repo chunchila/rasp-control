f648e08495e76647ea5772863febc4d861b5a395
romanrozin <r@commit.com>
2018-11-16 22:37:03 +0200

    dfdf

--- local/webSocket.py
+++ local/webSocket.py
@@ -2,6 +2,7 @@ from lomond import WebSocket
 from lomond.persist import persist
 websocket = WebSocket('wss://still-crag-29020.herokuapp.com/register')
 for event in persist(websocket):
+    print("got new event " , event)
     if event.name == 'text':
         print(event.text)
         #websocket.send(event.text)
