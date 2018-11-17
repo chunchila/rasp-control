3aefc0bb34088f80715a967d56ece6b4e1bedd99
romanrozin <r@commit.com>
2018-11-17 14:14:54 +0200

    dfdf

--- app.py
+++ app.py
@@ -9,6 +9,56 @@ import logging
 
 fileName = "tmpfile.txt"
 
+html = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+  
+  <form action="/change" method="get">
+<div class="container">
+  <h1>Click The Button To Change The Status</h1>        
+  <button type="submit" class="btn btn-success btn-lg">Primary</button>
+</div>
+
+</body>
+</html>
+'''
+
+changed_html = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <form action="/change" method="get">
+<div class="container">
+  <h1>Status Changed To {0}</h1>        
+  <h1>Click The Button To Change The Status</h1>        
+  <button type="submit" class="btn btn-success btn-lg">Primary</button>
+</div>
+
+</body>
+</html>
+
+'''
+
 if not os.path.exists(fileName):
     with open(fileName, "w+") as file:
         file.write("0")
@@ -26,16 +76,16 @@ def change_func_page():
         else:
             val = "1"
             file.write(val)
-    return (val)
+    return changed_html.replace("{0}" , val)
 
 @app.route('/')
 def hello():
     with open(fileName, "r") as file:
         data = file.read()
-    return data
+    return html
 
 
-@sockets.route('/registry')
+@sockets.route('/register')
 def reg(ws):
     print("im in socket")
     while not ws.closed:
@@ -49,5 +99,10 @@ def reg(ws):
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    #app.run(debug=True, port=5000, host="0.0.0.0")
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
 
